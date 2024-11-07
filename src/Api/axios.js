6085a75c681e564fd26fbc2d6f520ce51dc79e1e
ibsa-eng <104551852+ibsa-eng@users.noreply.github.com>
2024-11-07 09:44:29 -0600

    git commit

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-81748/us-central1/api",
+  //baseURL: "http://127.0.0.1:5001/clone-81748/us-central1/api",
+  baseURL: "https://amazon-api-deploy-9unc.onrender.com/api",
 });
 export { axiosInstance };
